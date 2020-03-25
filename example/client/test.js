23c97666e73940cc8d6a4621a3dfc839f124a059
Felipe Alfaro Solana <felipe.solana@logdna.com>
2020-03-25 18:00:29 +0100

    Latest updates to NodeJS Redis test client program.

--- example/client/test.js
+++ example/client/test.js
@@ -3,51 +3,68 @@
 
 var sprintf = require('sprintf-js').sprintf;
 var util = require("util");
-var Redis = require("ioredis");
-var redis = new Redis({
-  enableOfflineQueue: false,
-  showFriendlyErrorStack: true,
+const Redis = require("ioredis");
+const debug = require('debug')('redis_test');
+
+const REDIS_MAX_DELAY = 5000;
+
+const opts = {
   sentinels: [
     // This should be the FQDN of the `rfs-ldrs` Kubernetes Service
     // which should work as long as there is a Sentinel up and running
-        { host: "rfs-ldrs", port: 26379 },
-    //  { host: "10.233.48.161", port: 26379 },
+    {
+      host: "rfs-ldrs",
+      port: 26379
+    },
   ],
-  name: "mymaster"
-});
-
-redis.on('error', function(err) {
-  if (err.toString().indexOf('ECONNRESET') < 0 &&
-      err.toString().indexOf('ECONNREFUSED') < 0 &&
-      err.toString().indexOf('EHOSTUNREACH') < 0 &&
-      err.toString().indexOf('ETIMEDOUT') < 0) {
-    util.log(sprintf('Redis error: %s', err.toString()))
-  } else {
-    util.log(sprintf('Redis error: %s', (err.stack || '').split('\r\n')))
+  name: "mymaster",
+  autoResubscribe: true,
+  maxRetriesPerRequest: 1,  // only retry failed requests once
+  enableOfflineQueue: true, // queue offline requests
+  enableReadyCheck: true,
+  retryStrategy: function(times) {
+    return REDIS_MAX_DELAY;
+  },
+  reconnectOnError: function(err) {
+    // only reconnect on error if the node you are connected to
+    // has switched to READONLY mode
+    return err.message.startsWith('READONLY')
   }
-});
-redis.on('end', function() {
-  util.log('Redis  error: connection lost. No more retries.')
-});
-redis.on('reconnecting', function() {
-  util.log('Redis error: reconnecting error')
-});
-redis.on('ready', function() {
-  util.log('Connected to Redis.')
+};
+
+// make Redis connection
+const redis = new Redis(opts)
+.on('connect', () => {
+  debug('Redis connect')
+})
+.on('ready', () => {
+  debug('Redis ready')
+})
+.on('error', (e) => {
+  debug('Redis ready', e)
+})
+.on('close', () => {
+  debug('Redis close')
+})
+.on('reconnecting', () => {
+  debug('Redis reconnecting')
+})
+.on('end', () => {
+  debug('Redis end')
 });
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
-function dumpStats(key, iterations, current) {
-  util.log(sprintf("key: %-20s  total iterations: %-10d  current value: %-10d", key, iterations, current))
+function dumpStats(key, iterations, currentValue, previousValue) {
+  util.log(sprintf("key: %-20s  total iterations: %-10d  current value: %-10d  delta: %-3d", key, iterations, currentValue, currentValue - previousValue))
 }
 
 function onExit() {
-  redis.get(key).then(function(final) {
+  redis.get(key).then(function(finalValue) {
     util.log('exitting...')
-    dumpStats(key, iterations, final)
+    dumpStats(key, iterations, finalValue, previousValue)
     redis.del(key)
     redis.quit()
     process.exit(0)
@@ -58,15 +75,17 @@ process.on('SIGTERM', onExit)
 
 // Generate a random key to avoid collisions between multiple clients
 var iterations = 0
+var previousValue = 0
 var key = sprintf("test-key-%05d", getRandomInt(1000))
-dumpStats(key, iterations, 0)
+dumpStats(key, iterations, 0, 0)
 
 function dumpCounter() {
-  redis.get(key, function(err, current) {
+  redis.get(key, function(err, currentValue) {
     if (err != undefined) {
       console.log(err)
     } else {
-      dumpStats(key, iterations, current)
+      dumpStats(key, iterations, currentValue, previousValue)
+      previousValue = currentValue
     }
   })
 }
@@ -81,4 +100,4 @@ function incCounter() {
     }
   })
 }
-setInterval(incCounter, 10); //time is in ms
+setInterval(incCounter, 25); //time is in ms
